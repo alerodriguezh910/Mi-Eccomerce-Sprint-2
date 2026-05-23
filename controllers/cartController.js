ff18502e7729ad422bfc95a2abf3aefcd1af984e
Ezequiel Vilchez <vilchezezequiel9@gmail.com>
2026-05-22 23:02:23 -0300

    User Story #11-terminado

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const cartService = require('../services/cartService');
 
 const verCarrito = (req, res) => {
@@ -8,7 +10,25 @@ const verCarrito = (req, res) => {
 
 const agregarProducto = (req, res) => {
     const productId = parseInt(req.params.id);
-    cartService.agregarProducto(req.session, productId);
+
+    try {
+        const productsFilePath = path.join(__dirname, '../data/productos.json');
+        const productsData = fs.readFileSync(productsFilePath, 'utf-8');
+        const allProducts = JSON.parse(productsData);
+
+        const productoRequerido = allProducts.find(p => p.id === productId);
+
+        if (productoRequerido && productoRequerido.stock > 0) {
+            cartService.agregarProducto(req.session, productId);
+        } else {
+            console.log("Alerta de seguridad: Intento de agregar producto sin stock desde el front-end.");
+        }
+
+    } catch (error) {
+        console.error("Error al intentar validar el stock del producto:", error);
+    }
+
+
     res.redirect('/cart');
 };
 
